943af391a1307b1203121afedc8187567f93fecb
Sknight317 <Sknight23@mail.valenciacollege.edu>
2018-10-17 17:41:50 -0400

    added css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,22 @@
+ 
+var i = 0;
+var txt = 'Welcome to My Portfolio!!';
+var speed = 150; 
+
+// Typewriter function to display each letter in the text
+function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("intro").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  
+  // Document ready funtion to run the typewriter function after 2 seconds when the page is fully loaded
+  $(document).ready(function(){
+      setInterval(typeWriter, 2000)
+  });
+ 
  // Collapse Navbar
  var navbarCollapse = function() {
     if ($("#Nav").offset().top > 100) {
